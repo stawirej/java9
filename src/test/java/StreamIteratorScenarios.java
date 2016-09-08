57f7a20561dcbca232329117d0d89ec54e36a84a
Piotr Stawirej <piotr.stawirej@gmail.com>
2016-09-08 23:17:18 +0200

    Stream iterate - iterator-like-data.

--- src/test/java/StreamIteratorScenarios.java
+++ src/test/java/StreamIteratorScenarios.java
@@ -2,8 +2,10 @@ import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.BDDAssertions.then;
 
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 public class StreamIteratorScenarios {
@@ -21,5 +23,28 @@ public class StreamIteratorScenarios {
 
         // Then
         then(numbers).containsExactly(0, 1, 2, 3, 4);
+    }
+
+    /**
+     * Do not use to iterate through iterable like data as it lose last element.
+     */
+    @Test
+    public void shouldLoseLastElementWhenIterateThroughIteratorLikeData() {
+        // Given
+        final StringTokenizer tokens = new StringTokenizer("A B C D");
+        List<Object> letters = Lists.newArrayList();
+
+        // When
+        if (tokens.hasMoreElements()) {
+            letters = //
+                Stream //
+                    .iterate(tokens.nextElement(), //
+                        element -> tokens.hasMoreElements(), //
+                        element -> tokens.nextElement()) //
+                    .collect(toList());
+        }
+
+        // Then
+        then(letters).containsExactly("A", "B", "C");
     }
 }
