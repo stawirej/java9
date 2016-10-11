ef47741f94f69bd93d86ac46bed7dde1c9409b97
Piotr Stawirej <piotr.stawirej@gmail.com>
2016-10-11 23:16:18 +0200

    SafeVarargs on private moethods.

---
+++ src/test/java/SafeVarargsScenarios.java
@@ -0,0 +1,31 @@
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+import java.util.Optional;
+
+class SafeVarargsScenarios {
+
+    @Test
+    public void shouldGetFirstElement() {
+        //When
+        final Optional<Integer> first = first(1, 2, 3);
+
+        //Then
+        then(first.isPresent()).isTrue();
+        then(first.get()).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldGetEmptyOptional() {
+        //When
+        final Optional<Integer> first = first();
+
+        //Then
+        then(first.isPresent()).isFalse();
+    }
+
+    @SafeVarargs
+    private <T> Optional<T> first(T... arguments) {
+        return arguments.length == 0 ? Optional.empty() : Optional.of(arguments[0]);
+    }
+}
