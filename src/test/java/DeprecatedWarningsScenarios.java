7a9ff085f08da0e33bee35417d84d3e542ba4db8
Piotr Stawirej <piotr.stawirej@gmail.com>
2016-10-11 23:29:36 +0200

    No Deprecation Warnings for imports.

---
+++ src/test/java/DeprecatedWarningsScenarios.java
@@ -0,0 +1,9 @@
+import java.io.LineNumberInputStream;
+
+// Im Java 8 warning despite annotation.
+// In Java 9 not.
+@Deprecated
+class DeprecatedWarningsScenarios {
+
+    LineNumberInputStream lineNumberInputStream;
+}
